c7dcfddf76cc298951373905e9b324c774b6fcc5
Darrel Joris <joris.alima@agilion.ca>
2024-07-24 17:10:29 +0100

    Installed nuget dependencies

--- PocAzureFileShareIO/Program.cs
+++ PocAzureFileShareIO/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace PocAzureFileShareIO
+﻿using Azure.Storage;
+using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.IO;
+using System.Text;
+
+
+namespace PocAzureFileShareIO
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
