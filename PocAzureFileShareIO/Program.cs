6de06d175809cd4ed35b64f5cae91a4bf5067e29
Darrel Joris <joris.alima@agilion.ca>
2024-08-06 01:03:59 +0100

    customize the code

--- PocAzureFileShareIO/Program.cs
+++ PocAzureFileShareIO/Program.cs
@@ -29,11 +29,13 @@ namespace AzureFileShareIO
 
                     //Log into th File Share
                     ShareClient dummyShare = new(connectionString, shareName);
-                    Console.WriteLine("Connection to Azure Storage succeeded...");
+                    Console.WriteLine("Connection to Azure File Share succeeded...");
 
                     // Create a file of 50MB or more...
                     // Firstly check if the file we want to upload already exists
                     await dummyShare.CreateIfNotExistsAsync();
+                    Console.WriteLine($"File share {shareName} does not exist...");
+                    Console.WriteLine($"So, we created it...");
 
                     MemoryStream stream = new MemoryStream();
                     stream.Seek(500 * 1024 * 1024, SeekOrigin.Begin);
@@ -55,13 +57,34 @@ namespace AzureFileShareIO
 
                     // Create the file on the Azure FIle Share with the specified size
                     await dummyFileClient.CreateAsync(stream.Length);
+                    Console.WriteLine($"We have created {fileName} for you...");
 
                     // Upload the stream to the file in the Azure File Share, using the specified upload options
                     if (stream.Position < stream.Length)
                     {
+                        Console.WriteLine($"Upload of {fileName} in progress...\n");
                         await dummyFileClient.UploadAsync(stream, shareFileUploadOptions);
                         Console.WriteLine("Upload successful...");
-                    }              
+                    } else
+                {
+                    Console.WriteLine($"Upload of {fileName} has failed...");
+                }              
+            }
+
+
+            static async Task Main(string[] args)
+            {
+                Console.WriteLine("Welcome to the DummyFileAsync method");
+                Console.WriteLine("Enter the share name");
+                var dummyShareName = Console.ReadLine();
+
+                Console.WriteLine("Enter the file name");
+                var dummyFile = Console.ReadLine();
+
+                Console.WriteLine("Create and Upload operations done.");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+
             }
         }
     }
